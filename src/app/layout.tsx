0c14840962a34020c94caf559a56e148b75ddb44
Dayana <dayana2301@gmail.com>
2024-10-18 20:33:03 +0100

    feat: páginas y navegación

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,8 @@
 import type { Metadata } from "next";
 import localFont from "next/font/local";
 import "./globals.css";
+import Link from 'next/link';
+import Image from 'next/image';
 
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
@@ -25,10 +27,39 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
-        {children}
+      <body>
+        <nav className="bg-indigo-300 p-4 shadow">
+          <div className="container mx-auto flex justify-between items-center">
+            <div className="flex space-x-4">
+              <a href="https://heavyduty.builders/" target="_blank" >
+                <Image
+                  src="/logo.png"
+                  width={30}
+                  height={30}
+                  alt="Logo"
+                ></Image>
+              </a>
+              <Link href="/">
+                <button className="text-black hover:text-white font-semibold text-m">
+                  Inicio
+                </button>
+              </Link>
+              <Link href="/modal">
+                <button className="text-black hover:text-white font-semibold text-m">
+                  Modal
+                </button>
+              </Link>
+              <Link href="/formulario">
+                <button className="text-black hover:text-white font-semibold text-m">
+                  Formulario
+                </button>
+              </Link>
+            </div>
+          </div>
+        </nav>
+        <main className="flex min-h-screen flex-col items-center justify-between p-8 bg-white">
+          {children}
+        </main>
       </body>
     </html>
   );
